a95adbe0604a6423bd437bee2ffc2b436ee2f62f
Denis <vite0150@gmail.com>
2017-03-15 21:42:27 -0600

    Actualizado examen fisico ginecologia y agregado calendario al index

--- src/java/com/sicom/web/beans/CitasBean.java
+++ src/java/com/sicom/web/beans/CitasBean.java
@@ -3,9 +3,25 @@ package com.sicom.web.beans;
 import com.sicom.controller.CitaJpaController;
 import com.sicom.entities.Cita;
 import java.util.ArrayList;
+import javax.persistence.Persistence;
+ 
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.persistence.Persistence;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
 
 /**
  *
@@ -13,11 +29,15 @@ import javax.persistence.Persistence;
  */
 @ManagedBean
 @ViewScoped
-public class CitasBean {
+public class CitasBean implements Serializable{
     private Cita nuevaCita;
     private Cita selectedCita;
     private ArrayList<Cita> listaCitas;
     private final CitaJpaController cjc;
+    private ScheduleModel eventModel;
+     
+ 
+    private ScheduleEvent event = new DefaultScheduleEvent();
 
     public CitasBean() {
         cjc = new CitaJpaController(Persistence.createEntityManagerFactory("SICOM_v1PU"));
@@ -69,5 +89,84 @@ public class CitasBean {
      */
     public void setListaCitas(ArrayList<Cita> listaCitas) {
         this.listaCitas = listaCitas;
+    }
+    
+    
+    
+    @PostConstruct
+    public void init() {
+        eventModel = new DefaultScheduleModel();
+        
+         
+    }
+     
+    public Date getRandomDate(Date base) {
+        Calendar date = Calendar.getInstance();
+        date.setTime(base);
+        date.add(Calendar.DATE, ((int) (Math.random()*30)) + 1);    //set random day of month
+         
+        return date.getTime();
+    }
+     
+    public Date getInitialDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+         
+        return calendar.getTime();
+    }
+     
+    public ScheduleModel getEventModel() {
+        return eventModel;
+    }
+     
+ 
+    private Calendar today() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
+ 
+        return calendar;
+    }
+     
+   
+     
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+ 
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
+    }
+     
+    public void addEvent(ActionEvent actionEvent) {
+        if(event.getId() == null)
+            eventModel.addEvent(event);
+        else
+            eventModel.updateEvent(event);
+         
+        event = new DefaultScheduleEvent();
+    }
+     
+    public void onEventSelect(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+    }
+     
+    public void onDateSelect(SelectEvent selectEvent) {
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+    }
+     
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita Movida", event.getDayDelta() + " dias despues" + " y " + event.getMinuteDelta() + " minutos despues");
+         
+        addMessage(message);
+    }
+     
+    public void onEventResize(ScheduleEntryResizeEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita Movida", event.getDayDelta() + " dias despues" + " y " + event.getMinuteDelta() + " minutos despues");
+         
+        addMessage(message);
+    }
+     
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 }
--- src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
+++ src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
@@ -1,31 +1,56 @@
 package com.sicom.web.beans;
 
+import com.sicom.controller.ExamenGinecologiaJpaController;
 import com.sicom.controller.ValorJpaController;
+import com.sicom.entities.ExamenGinecologia;
+import com.sicom.entities.Paciente;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ViewScoped
 public class ExamenFisicoGinecBean {
 
+    private ExamenGinecologia examenFisico;
     private Date hoy;
-    private final ValorJpaController vjc;
+    private ExamenGinecologiaJpaController ejc;
+    private ValorJpaController vjc;
+    private UploadedFile file;
+    private Paciente paciente;
+    private StreamedContent imagen;
+    private String bus;
+    private byte[] imageContents;
 
-    public ExamenFisicoGinecBean(){
+    public ExamenFisicoGinecBean() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("SICOM_v1PU");
         vjc = new ValorJpaController(emf);
-    
+        ejc = new ExamenGinecologiaJpaController(emf);
+        imagen = null;
+        bus = "";
+        examenFisico = new ExamenGinecologia();
+        paciente = PacienteBean.getSavedPaciente();
+        imageContents=null;
         hoy = new Date();
+        file = null;
     }
 
     public List<String> consultarValoresPorCodigo(Integer codigo) {
         return this.vjc.findByCodeId(codigo);
     }
-    
+
     public void setHoy(Date hoy) {
         this.hoy = hoy;
     }
@@ -33,6 +58,128 @@ public class ExamenFisicoGinecBean {
     public Date getHoy() {
         return hoy;
     }
-    
-}
 
+    public ExamenGinecologia getExamenFisico() {
+        return examenFisico;
+    }
+
+    public void setExamenFisico(ExamenGinecologia examenFisico) {
+        this.examenFisico = examenFisico;
+    }
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public String getBus() {
+        return bus;
+    }
+
+    public void setBus(String bus) {
+        this.bus = bus;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public StreamedContent getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(StreamedContent imagen) {
+        this.imagen = imagen;
+    }
+
+    public byte[] getImageContents() {
+        return imageContents;
+    }
+
+    public void setImageContents(byte[] imageContents) {
+        this.imageContents = imageContents;
+    }
+
+    public void agregar(){
+        try {
+            examenFisico.setBus("Si".equals(bus));
+            examenFisico.setImagenMamas(file.getContents());
+            examenFisico.setExpedientePacientecedula(paciente.getExpediente());
+            ejc.create(examenFisico);
+
+        } catch (Exception ex) {
+            Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void modificar(){
+        try{
+        ejc.edit(examenFisico);
+        }
+        catch (Exception ex) {
+            Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void save() {
+        try {
+            agregar();
+            examenFisico = new ExamenGinecologia();
+            volver_a_informacion();
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Examen Agregado","Examen Agregado Correctamente");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+
+        } catch (Exception ex) {
+
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Error","Error, examen No Se Pudo Agregar ");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void uploadListener(FileUploadEvent e) {
+        file = e.getFile();
+        
+        if (file != null) {
+            imageContents=file.getContents();
+//            try {
+//                imagen = new DefaultStreamedContent(new ByteArrayInputStream(file.getContents()));
+//                FacesContext fc = FacesContext.getCurrentInstance();
+//                ExternalContext ec = fc.getExternalContext();
+//                
+//                file.write(ec.getApplicationContextPath()+"/imagenes/"+file.getFileName());
+//                
+//            } catch (Exception ex) {
+//                Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Imagen Subida","La imagen " + file.getFileName() + " se ha subido.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+
+    }
+    
+    public String getImageContentsAsBase64() {
+        return Base64.getEncoder().encodeToString(file.getContents());
+    }
+
+    public void volver_a_informacion() {
+        try {
+
+            FacesContext fc = FacesContext.getCurrentInstance();
+            ExternalContext ec = fc.getExternalContext();
+
+            String URL = ec.getRequestContextPath() + "/app/paciente/informacion";
+
+            ec.redirect(URL);
+
+        } catch (Exception ex) {
+        }
+    }
+
+}
