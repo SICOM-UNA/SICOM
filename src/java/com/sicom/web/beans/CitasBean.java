8f7a4020ea82dd7443f5a7623ca61a045b4bee3e
luisf96v <luisf96v@outlook.com>
2017-05-13 14:55:46 -0600

    Actualizacion

--- src/java/com/sicom/web/beans/CitasBean.java
+++ src/java/com/sicom/web/beans/CitasBean.java
@@ -4,7 +4,6 @@ import com.sicom.controller.CitaJpaController;
 import com.sicom.entities.Cita;
 import com.sicom.entities.Departamento;
 import com.sicom.entities.Login;
-import com.sicom.entities.Personal;
 import java.io.IOException;
 import javax.persistence.Persistence;
 
@@ -13,16 +12,17 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpServletRequest;
+import org.joda.time.DateTime;
 
 import org.primefaces.event.ScheduleEntryMoveEvent;
 import org.primefaces.event.ScheduleEntryResizeEvent;
@@ -40,6 +40,8 @@ import org.primefaces.model.ScheduleModel;
 @ViewScoped
 public class CitasBean implements Serializable {
 
+    private final int DURACION_CITA_MINUTOS = 30;
+
     private Cita nuevaCita;
     private Cita selectedCita;
     private List<Cita> listaCitas;
@@ -47,57 +49,92 @@ public class CitasBean implements Serializable {
     private ScheduleModel eventModel;
     private Departamento departamento;
 
-    private ScheduleEvent event = new DefaultScheduleEvent();
+    private ScheduleEvent event;
 
     public CitasBean() {
         cjc = new CitaJpaController(Persistence.createEntityManagerFactory("SICOM_v1PU"));
 
-        nuevaCita = new Cita();
-        selectedCita = new Cita();
+        String url = ((HttpServletRequest) (FacesContext.getCurrentInstance().getExternalContext().getRequest())).getRequestURL().toString();
+        url = url.substring(url.lastIndexOf("/") + 1);
 
-        listaCitas = cjc.findCitaEntities();
+        switch (url) {
+            case "seleccion": {
+                this.validacion_redirect(); //
+                break;
+            }
+            case "citas":
+            case "index": {
+                this.init_Datos();
+                break;
+            }
 
-        FacesContext fc = FacesContext.getCurrentInstance();
-        ExternalContext ec = fc.getExternalContext();
-
-        departamento = (Departamento) ec.getSessionMap().remove("departamento");
+        }
     }
 
-    @PostConstruct
-    public void init() {
-        FacesContext fc = FacesContext.getCurrentInstance();
-        ExternalContext ec = fc.getExternalContext();
+    /*METODOS DEL CONSTRUCTOR*/
+    private void init_Citas() {
+        boolean generaCitas = false;
 
-        Login login = (Login) ec.getSessionMap().get("login");
-        Personal p = login.getPersonal();
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
 
-        if (p != null) {
-            int dpto = p.getDepartamentoId().getId();
+        if (departamento != null) {
+            generaCitas = true;
+        } else {
+            Departamento dpto = ((Login) ec.getSessionMap().get("login")).getPersonal().getDepartamentoId();
 
-            if (dpto != 4 && dpto != 1 && departamento == null) {
-                try {
-                    departamento = p.getDepartamentoId();
-                    String URL = ec.getRequestContextPath() + "/app/calendario/citas";
-                    ec.getSessionMap().put("departamento", departamento);
-                    ec.redirect(URL);
-                } catch (IOException ex) {
-                    Logger.getLogger(CitasBean.class.getName()).log(Level.SEVERE, null, ex);
+            if (dpto.getId() != 4 && dpto.getId() != 1) {
+                departamento = dpto;
+                generaCitas = true;
+            }
+        }
+        if (generaCitas) {
+            int id = departamento.getId();
+
+            listaCitas = cjc.findCitaEntities();
+            for (Cita c : listaCitas) {
+                if (c.getDepartamentoid().getId() == id) {
+                    DefaultScheduleEvent dse = new DefaultScheduleEvent(c.getNombre(), c.getFechaInicial(), c.getFechaFinal(), c);
+                    this.eventModel.addEvent(dse);
                 }
-            } else {
-                if (departamento != null) {
-                    eventModel = new DefaultScheduleModel();
-
-                    for (Cita c : this.listaCitas) {
-                        if (c.getDepartamentoid().getId() == this.departamento.getId()) {
-                            this.eventModel.addEvent(new DefaultScheduleEvent(c.getNombre(), c.getFechaInicial(), c.getFechaFinal()));
-                        }
-                    }
-                }
+            }
+        } else {
+            try {
+                ec.redirect(ec.getRequestContextPath().concat("/app/calendario/seleccion"));
+            } catch (IOException ex) {
+                Logger.getLogger(CitasBean.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 
-    public void redirigir_a_Citas() {
+    private void init_Datos() {
+        eventModel = new DefaultScheduleModel();
+        event = new DefaultScheduleEvent();
+        departamento = (Departamento) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("departamento");
+        selectedCita = new Cita();
+        nuevaCita = new Cita();
+        this.init_Citas();
+    }
+
+    private void validacion_redirect() {
+
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        Departamento dpto = ((Login) ec.getSessionMap().get("login")).getPersonal().getDepartamentoId();
+
+        if (dpto.getId() != 4 && dpto.getId() != 1) {
+            try {
+                departamento = dpto;
+                String URL = ec.getRequestContextPath() + "/app/calendario/citas";
+                ec.getSessionMap().put("departamento", departamento);
+                ec.redirect(URL);
+            } catch (IOException ex) {
+                Logger.getLogger(CitasBean.class
+                        .getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    /*--------------------------------------------------------------------------------*/
+    public void redireccion_a_Citas() {
         try {
             FacesContext fc = FacesContext.getCurrentInstance();
             ExternalContext ec = fc.getExternalContext();
@@ -126,82 +163,92 @@ public class CitasBean implements Serializable {
     }
 
     public void addEvent(ActionEvent actionEvent) {
+        FacesMessage message;
+
         if (event.getId() == null) { //Si se va a agregar un evento nuevo
             try {
-                nuevaCita.setFechaInicial(event.getStartDate()); //se deben tomar estos atributos del event porque si no la interfaz del calendario no muestra los datos
-                nuevaCita.setFechaFinal(event.getEndDate());
-                nuevaCita.setNombre(event.getTitle());
+                Date endDate = new DateTime(event.getStartDate()).plusMinutes(DURACION_CITA_MINUTOS).toDate();
 
-                //nuevaCita.setDepartamentoid(new Departamento(2, "Ginecología")); //********* Cambiar*************************
+                nuevaCita.setFechaInicial(event.getStartDate());
+                nuevaCita.setFechaFinal(endDate);
+                nuevaCita.setNombre(event.getTitle());
                 nuevaCita.setDepartamentoid(departamento);
+
                 cjc.create(nuevaCita);
                 listaCitas.add(nuevaCita);
-                nuevaCita = new Cita();
-                eventModel.addEvent(event);
-                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita agregada", "Cita de " + nuevaCita.getNombre() + " agregada correctamente.");
-                addMessage(message);
 
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita agregada", "Cita agregada correctamente.");
             } catch (Exception ex) {
-                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "La cita de " + nuevaCita.getNombre() + " no se pudo agregar");
-                addMessage(message);
-                System.err.println(ex.getStackTrace());
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "La cita no se pudo agregar");
             }
-
         } else { //Al actualizar una cita
             try {
+                Date endDate = new DateTime(event.getStartDate()).plusMinutes(DURACION_CITA_MINUTOS).toDate();
+
+                selectedCita = (Cita) event.getData();
                 selectedCita.setFechaInicial(event.getStartDate());
-                selectedCita.setFechaFinal(event.getEndDate());
+                selectedCita.setFechaFinal(endDate);
                 selectedCita.setNombre(event.getTitle());
+                selectedCita.setMotivo(nuevaCita.getMotivo());
+                selectedCita.setTelefono(nuevaCita.getTelefono());
+
                 cjc.edit(selectedCita);
                 selectedCita = null;
-                nuevaCita = new Cita();
-                eventModel.updateEvent(event);
+
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita moficada", "La cita se actualizo correctamente.");
             } catch (Exception ex) {
-                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "La cita de " + nuevaCita.getNombre() + " no se pudo editar");
-                addMessage(message);
-                System.err.println(ex.getStackTrace());
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "La cita no se pudo editar");
             }
-
         }
-
+        this.addMessage(message);
+        nuevaCita = new Cita();
         event = new DefaultScheduleEvent();
+        actualizaCitas();
     }
 
     public void onEventSelect(SelectEvent selectEvent) {
         event = (ScheduleEvent) selectEvent.getObject();
-        nuevaCita = buscarCitaPorFechas();
+        nuevaCita = buscarCita();
         selectedCita = nuevaCita;
-
     }
 
     public void onDateSelect(SelectEvent selectEvent) {
-        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
-        selectedCita = null;
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(),null);
+        nuevaCita = new Cita();
     }
 
     public void onEventMove(ScheduleEntryMoveEvent event) {
+               
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita Movida", event.getDayDelta() + " dias" + " y " + event.getMinuteDelta() + " minutos");
-
         addMessage(message);
     }
 
-    public void onEventResize(ScheduleEntryResizeEvent event) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cita Movida", event.getDayDelta() + " dias" + " y " + event.getMinuteDelta() + " minutos");
-
-        addMessage(message);
-    }
 
     private void addMessage(FacesMessage message) {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    public Cita buscarCitaPorFechas() {
+    public Cita buscarCita() {
         for (Cita c : listaCitas) {
-            if (c.getFechaInicial().compareTo(event.getStartDate()) == 0 && c.getFechaFinal().compareTo(event.getEndDate()) == 0) {
+            int id = ((Cita) event.getData()).getId();
+            if (Objects.equals(c.getId(), id)) {
                 return c;
             }
         }
-        return null;
+        return new Cita();
+    }
+
+    public void actualizaCitas() {
+
+        int id = departamento.getId();
+        listaCitas = cjc.findCitaEntities();
+        eventModel.clear();
+        for (Cita c : listaCitas) {
+            if (c.getDepartamentoid().getId() == id) {
+                DefaultScheduleEvent dse = new DefaultScheduleEvent(c.getNombre(), c.getFechaInicial(), c.getFechaFinal(), c);
+                this.eventModel.addEvent(dse);
+            }
+        }
     }
 
     /**
