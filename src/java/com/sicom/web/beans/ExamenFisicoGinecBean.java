217cd26ce36f545b0d0bd992e058cd2916995095
luisf96v <luisf96v@outlook.com>
2017-05-14 16:32:45 -0600

    Actualizacion examen fisico

--- src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
+++ src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
@@ -3,40 +3,43 @@ package com.sicom.web.beans;
 import com.sicom.controller.ExamenGinecologiaJpaController;
 import com.sicom.entities.ExamenGinecologia;
 import com.sicom.entities.Paciente;
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.Base64;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ViewScoped
-public class ExamenFisicoGinecBean implements Serializable{
+public class ExamenFisicoGinecBean implements Serializable {
+
     private ExamenGinecologia examenFisico;
     private Date hoy;
     private ExamenGinecologiaJpaController ejc;
     private Paciente paciente;
-    private String bus;
-    private UploadedFile file;
 
     public ExamenFisicoGinecBean() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SICOM_v1PU");
-        ejc = new ExamenGinecologiaJpaController(emf);
-        bus = "";
+        ejc = new ExamenGinecologiaJpaController(Persistence.createEntityManagerFactory("SICOM_v1PU"));
         examenFisico = new ExamenGinecologia();
-        file=null;
-        hoy = new Date();
+
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        paciente = (Paciente) ec.getSessionMap().get("paciente");
+
+        System.out.println(paciente.getNombre());
+        
+        if (paciente == null) {
+            try {
+                ec.redirect(ec.getRequestContextPath().concat("/app/paciente/consultar"));
+            } catch (IOException ex) {
+                Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public void setHoy(Date hoy) {
@@ -55,15 +58,6 @@ public class ExamenFisicoGinecBean implements Serializable{
         this.examenFisico = examenFisico;
     }
 
-
-    public String getBus() {
-        return bus;
-    }
-
-    public void setBus(String bus) {
-        this.bus = bus;
-    }
-
     public Paciente getPaciente() {
         return paciente;
     }
@@ -72,68 +66,61 @@ public class ExamenFisicoGinecBean implements Serializable{
         this.paciente = paciente;
     }
 
-    public UploadedFile getFile() {
-        return file;
-    }
+    public void modificar() {
+        try {
+            ejc.edit(examenFisico);
 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-
-    
-    
-    public void modificar(){
-        try{
-        ejc.edit(examenFisico);
-        }
-        catch (Exception ex) {
-            Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(ExamenFisicoGinecBean.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public void save() {
         try {
-            try {
-            examenFisico.setBus("Si".equals(bus));
-            if(file!=null){
-                examenFisico.setImagenMamas(file.getContents());
+            try {/*
+                examenFisico.setBus("Si".equals(bus));
+                if (file != null) {
+                    examenFisico.setImagenMamas(file.getContents());
+                }
+                examenFisico.setExpedientePacientecedula(paciente.getExpediente());
+                ejc.create(examenFisico);
+                 */
+            } catch (Exception ex) {
+                Logger.getLogger(ExamenFisicoGinecBean.class
+                        .getName()).log(Level.SEVERE, null, ex);
             }
-            examenFisico.setExpedientePacientecedula(paciente.getExpediente());
-            ejc.create(examenFisico);
-
-        } catch (Exception ex) {
-            Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
             examenFisico = new ExamenGinecologia();
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Examen Agregado","Examen Agregado Correctamente");
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Examen Agregado", "Examen Agregado Correctamente");
             FacesContext.getCurrentInstance().addMessage(null, message);
 
         } catch (Exception ex) {
 
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Error","Error, examen No Se Pudo Agregar ");
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "Error, examen No Se Pudo Agregar ");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
 
+    /*
     public void uploadListener(FileUploadEvent e) {
         file = e.getFile();
-        
+
         if (file != null) {
 //                String str = e.getFile().getFileName();
 //                String ext = str.substring(str.lastIndexOf('.'), str.length()); //obtener la extension
 //                String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
-                
-                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,"Imagen Subida","La imagen " + file.getFileName() + " se ha subido.");
-                FacesContext.getCurrentInstance().addMessage(null, message);
-                
+
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Imagen Subida", "La imagen " + file.getFileName() + " se ha subido.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+
         }
 
     }
-    
+
     public String getImageContentsAsBase64() {
         return Base64.getEncoder().encodeToString(file.getContents());
     }
-
+     */
     public void volver_a_informacion() {
         try {
 
