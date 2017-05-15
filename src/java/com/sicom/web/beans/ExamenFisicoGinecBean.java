a08e7864beb6b0fcdebc97fe7fcf878c8c650dd0
luisf96v <luisf96v@outlook.com>
2017-05-14 21:52:17 -0600

    Actualizacion examen fisico

--- src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
+++ src/java/com/sicom/web/beans/ExamenFisicoGinecBean.java
@@ -2,7 +2,9 @@ package com.sicom.web.beans;
 
 import com.sicom.controller.ExamenGinecologiaJpaController;
 import com.sicom.entities.ExamenGinecologia;
+import com.sicom.entities.Login;
 import com.sicom.entities.Paciente;
+import com.sicom.entities.Personal;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
@@ -30,15 +32,54 @@ public class ExamenFisicoGinecBean implements Serializable {
 
         ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         paciente = (Paciente) ec.getSessionMap().get("paciente");
+        examenFisico.setPersonalcedula(((Login) ec.getSessionMap().get("login")).getPersonal());
 
-        System.out.println(paciente.getNombre());
-        
         if (paciente == null) {
             try {
                 ec.redirect(ec.getRequestContextPath().concat("/app/paciente/consultar"));
             } catch (IOException ex) {
                 Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+    }
+
+    public void save() {
+        try {
+            examenFisico.setFecha(new Date());
+            examenFisico.setExpedientePacientecedula(paciente.getExpediente());
+            FacesMessage message;
+
+            if (examenFisico.getId() == null) {
+                ejc.create(examenFisico);
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Examen Físico agregado exitosamente.",null);
+            } else {
+                ejc.edit(examenFisico);
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Examen Físico modificado exitosamente.",null);
+            }
+            
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            ec.getFlash().setKeepMessages(true);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            ec.redirect(ec.getRequestContextPath().concat("/app/paciente/informacion"));
+
+        } catch (Exception ex) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "El examen no se pudo agregar.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            Logger.getLogger(ExamenFisicoGinecBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void volver_a_informacion() {
+        try {
+
+            FacesContext fc = FacesContext.getCurrentInstance();
+            ExternalContext ec = fc.getExternalContext();
+
+            String URL = ec.getRequestContextPath() + "/app/paciente/informacion";
+
+            ec.redirect(URL);
+
+        } catch (Exception ex) {
         }
     }
 
@@ -65,74 +106,4 @@ public class ExamenFisicoGinecBean implements Serializable {
     public void setPaciente(Paciente paciente) {
         this.paciente = paciente;
     }
-
-    public void modificar() {
-        try {
-            ejc.edit(examenFisico);
-
-        } catch (Exception ex) {
-            Logger.getLogger(ExamenFisicoGinecBean.class
-                    .getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void save() {
-        try {
-            try {/*
-                examenFisico.setBus("Si".equals(bus));
-                if (file != null) {
-                    examenFisico.setImagenMamas(file.getContents());
-                }
-                examenFisico.setExpedientePacientecedula(paciente.getExpediente());
-                ejc.create(examenFisico);
-                 */
-            } catch (Exception ex) {
-                Logger.getLogger(ExamenFisicoGinecBean.class
-                        .getName()).log(Level.SEVERE, null, ex);
-            }
-            examenFisico = new ExamenGinecologia();
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Examen Agregado", "Examen Agregado Correctamente");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-
-        } catch (Exception ex) {
-
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "Error, examen No Se Pudo Agregar ");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
-    }
-
-    /*
-    public void uploadListener(FileUploadEvent e) {
-        file = e.getFile();
-
-        if (file != null) {
-//                String str = e.getFile().getFileName();
-//                String ext = str.substring(str.lastIndexOf('.'), str.length()); //obtener la extension
-//                String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
-
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Imagen Subida", "La imagen " + file.getFileName() + " se ha subido.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-
-        }
-
-    }
-
-    public String getImageContentsAsBase64() {
-        return Base64.getEncoder().encodeToString(file.getContents());
-    }
-     */
-    public void volver_a_informacion() {
-        try {
-
-            FacesContext fc = FacesContext.getCurrentInstance();
-            ExternalContext ec = fc.getExternalContext();
-
-            String URL = ec.getRequestContextPath() + "/app/paciente/informacion";
-
-            ec.redirect(URL);
-
-        } catch (Exception ex) {
-        }
-    }
-
 }
